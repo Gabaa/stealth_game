06129b38ee4dd544fac2755c625aeeb12b4d18ca
Mikkel Erfurth Gaba <mikkel@gaba.name>
2021-01-20 20:01:36 +0100

    end area is slightly transparent

--- src/colors.rs
+++ src/colors.rs
@@ -1,6 +1,6 @@
 use ggez::graphics::Color;
 
-pub const END_AREA: Color = Color::new(0.0, 1.0, 0.0, 1.0);
+pub const END_AREA: Color = Color::new(0.0, 1.0, 0.0, 0.1);
 
 pub const PLAYER_VISIBLE_AREA: Color = Color::new(1.0, 1.0, 1.0, 0.1);
 pub const GUARD_VISIBLE_AREA: Color = Color::new(1.0, 0.0, 0.0, 0.1);
