299e41826218c8baea4ca6084e847cc7bd083b6a
Gabaa <7581436+Gabaa@users.noreply.github.com>
2024-03-13 19:54:06 +0100

    WIP level system

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn main() {
 enum GameState {
     #[default]
     Menu,
+    Levels,
     Game,
 }
 
@@ -87,6 +88,128 @@ mod menu {
     }
 
     fn destroy_menu(entities: Query<Entity, With<MenuScreenRoot>>, mut commands: Commands) {
+        for entity in &entities {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+
+    fn button_system(
+        mut interaction_query: Query<
+            (&Interaction, &mut BackgroundColor, &MenuButtonAction),
+            (Changed<Interaction>, With<Button>),
+        >,
+        mut game_state: ResMut<NextState<GameState>>,
+        mut exit: EventWriter<AppExit>,
+    ) {
+        for (interaction, mut color, button_option) in &mut interaction_query {
+            match *interaction {
+                Interaction::Clicked => {
+                    *color = PRESSED_BUTTON.into();
+                    button_option.apply(&mut game_state, &mut exit);
+                }
+                Interaction::Hovered => {
+                    *color = HOVERED_BUTTON.into();
+                }
+                Interaction::None => {
+                    *color = NORMAL_BUTTON.into();
+                }
+            }
+        }
+    }
+
+    fn button(
+        parent: &mut ChildBuilder,
+        button_option: MenuButtonAction,
+        asset_server: &AssetServer,
+    ) {
+        parent
+            .spawn((
+                button_option,
+                ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                },
+            ))
+            .with_children(|parent| {
+                parent.spawn(TextBundle::from_section(
+                    format!("{button_option:?}"),
+                    TextStyle {
+                        font: asset_server.load("fonts/Roboto-Regular.ttf"),
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                ));
+            });
+    }
+}
+
+mod levels {
+    use bevy::{app::AppExit, prelude::*};
+
+    use crate::GameState;
+
+    const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+    const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+    const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+    pub struct LevelsPlugin;
+
+    impl Plugin for LevelsPlugin {
+        fn build(&self, app: &mut App) {
+            app.add_system(setup_levels.in_schedule(OnEnter(GameState::Levels)));
+            app.add_system(destroy_levels.in_schedule(OnExit(GameState::Levels)));
+            app.add_system(button_system.in_set(OnUpdate(GameState::Levels)));
+        }
+    }
+
+    #[derive(Component)]
+    struct LevelsScreenRoot;
+
+    fn setup_levels(mut commands: Commands, asset_server: Res<AssetServer>) {
+        asset_server.load("levels")
+        fn get_all_level_names() -> Vec<String> {
+            let levels_directory = Path::new("levels");
+            fs::read_dir(levels_directory)
+                .expect("Could not read levels")
+                .map(|level| {
+                    level
+                        .expect("Failed to read level")
+                        .file_name()
+                        .into_string()
+                        .expect("Could not read level name")
+                })
+                .map(|name| name.replace(".json", ""))
+                .collect()
+        }
+
+        commands
+            .spawn((
+                LevelsScreenRoot,
+                NodeBundle {
+                    style: Style {
+                        size: Size::width(Val::Percent(100.0)),
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        flex_direction: FlexDirection::Column,
+                        ..default()
+                    },
+                    ..default()
+                },
+            ))
+            .with_children(|parent| {
+                button(parent, MenuButtonAction::Start, &asset_server);
+                button(parent, MenuButtonAction::Editor, &asset_server);
+                button(parent, MenuButtonAction::Quit, &asset_server);
+            });
+    }
+
+    fn destroy_levels(entities: Query<Entity, With<MenuScreenRoot>>, mut commands: Commands) {
         for entity in &entities {
             commands.entity(entity).despawn_recursive();
         }
